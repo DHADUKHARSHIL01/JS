376ed502153b10649057147858634d15d902dd71
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-05 11:51:42 +0530

    add numbers_and_maths.js

---
+++ numbers_and_maths.js
@@ -0,0 +1,21 @@
+const number = 235;
+console.log(number);
+
+const Num1 = new Number(444);
+console.log(Num1);
+
+console.log(Num1.toString().length);
+console.log(Num1.toFixed(3));
+
+// *********** Maths ***********
+
+console.log(Math);
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const Min = 1;
+const Max = 6;
+
+console.log(Math.floor(Math.random() * (Max - Min + 1)) + Min);
