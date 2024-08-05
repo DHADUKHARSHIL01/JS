f4b3aa1f66f5d96300ea7afa17b05cdff8a1c4b8
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-05 11:24:51 +0530

    changes inconversionOperation.js add strings.js add comparison.js

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,2 +1,47 @@
-let number = 325;
-number.toString()
+let number = "132";
+
+console.log(typeof number);
+console.log(typeof number);
+
+let valueInNumber = Number(number); // convert string to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let num = 0;
+let BooleanNum = Boolean(num); //number to Boolean
+console.log(BooleanNum);
+
+//num is == 1 => value true
+// num is == 0 => value false
+
+let newnum = 10
+
+let stringnum = String(newnum) // convert Number to string
+console.log(typeof stringnum);
+console.log(stringnum);
+
+
+
+//  Operations 
+
+let number1 = 54
+let negnum = -number1
+
+console.log(negnum);
+
+let str1 = "dhaduk "
+let str2 = "harshil"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(true); //output true
+console.log(+true); //output 1
+console.log(+false); //output 0
+console.log(+""); //output 0
+
+let myNum = 12
+myNum++ // 12 + 1 output = 13
+console.log(myNum);
+
+
---
+++ strings.js
@@ -0,0 +1,25 @@
+let myName = "Harshil";
+let myAge = 20;
+
+console.log(`Hello, My Name is ${myName} and My Age is ${myAge}`);
+
+let fullname = "Harshil Dhaduk";
+
+console.log(fullname.toUpperCase());
+console.log(fullname.toLowerCase());
+console.log(fullname.length);
+console.log(fullname.indexOf(`h`));
+console.log(fullname.charAt(8));
+
+let Name = fullname.substring(0, 4);
+console.log(Name);
+
+let newstr = new String('   Harshil   ')
+console.log(newstr.trim());
+
+let newstr1 = new String('   Harshil   Dhaduk   ')
+console.log(newstr1.trim());
+
+
+
+
