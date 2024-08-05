737bc073bcbb7effc98e5f492a9797912824dae2
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-05 16:10:27 +0530

    changes

--- 02_arrays.js
+++ 02_arrays.js
@@ -1,14 +1,20 @@
-const marvel_Heroes =['Thor', 'Hulk', 'Black Panther' ]
-const dc_Heroes = ['Flash', 'Batman', 'Superman']
+const marvel_Heroes = ["Thor", "Hulk", "Black Panther"];
+const dc_Heroes = ["Flash", "Batman", "Superman"];
 
-const all_Heroes =marvel_Heroes.concat(dc_Heroes)
-
+const all_Heroes = marvel_Heroes.concat(dc_Heroes);
 console.log(all_Heroes);
 
-
-const all_Hero=[...marvel_Heroes, ...dc_Heroes]
-
+const all_Hero = [...marvel_Heroes, ...dc_Heroes];
 console.log(all_Hero);
 
+const arr01 = [12, 15, 20, [26, 5, 63, [45, 89], [65, 10, 20]], [40, 50, 60]];
+const arr02 = arr01.flat(Infinity);
+console.log(arr02);
 
+console.log(Array.isArray("Harshil"));
+console.log(Array.from("HarshilDhaduk"));
 
+let s1 = 100;
+let s2 = 200;
+let s3 = 300;
+console.log(Array.of(s1, s2, s3));
