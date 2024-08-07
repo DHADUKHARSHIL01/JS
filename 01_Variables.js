ada43b171f6a62aa1596e0fd043ce4d0bb9af605
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-07 18:16:21 +0530

    variables

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,13 +1,12 @@
 var userName = "harshil";
 let password = 1234;
 city = "surat";
-const accountNum = 56987230
+const accountNum = 56987230;
 
-userName ="Harshil Dhaduk";
-password = 2345
-city ="Rajkot"
+userName = "Harshil Dhaduk";
+password = 2345;
+city = "Rajkot";
 //accountNum = 23598 // not change value
-
 
 console.log(userName);
 console.log(password);
