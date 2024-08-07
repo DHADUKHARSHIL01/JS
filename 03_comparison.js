d272676db0f9df2c85140c2ae1b9bcdc8544defb
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-07 18:17:00 +0530

    comparison

--- 03_comparison.js
+++ 03_comparison.js
@@ -1,16 +1,13 @@
 console.log("2" > 1);
 console.log("02" > 1);
 
-
 console.log(null > 0);
 console.log(null >= 0);
 console.log(null == 0);
 
-
 console.log(undefined > 0);
 console.log(undefined >= 0);
 console.log(undefined == 0);
-
 
 //check data type
 console.log("4" === 4);
