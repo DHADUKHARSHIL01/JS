1bd15844de03b7582185b5037fab0fe866ef808f
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-07 15:17:31 +0530

    if

---
+++ 15_if.js
@@ -0,0 +1,22 @@
+if (3 != "3") {
+  console.log("Execute the Code 1.");
+}
+
+if (3 !== "3") {
+  console.log("Execute the Code 2.");
+}
+
+const speed = 100;
+if (speed >= 85) {
+  console.log("Your speed is veri fast");
+}
+
+const marks = 65;
+if (marks >= 33) {
+  console.log("you are a pass");
+}
+
+const password = "Passsword";
+if (password !== "P@ssword") {
+  console.log("Enter a valid password");
+}
