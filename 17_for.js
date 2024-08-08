4b8212e3c591bee1f8e0b4895bb0a7771ec6914c
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-08 09:22:11 +0530

    17_for

--- 17_for.js
+++ 17_for.js
@@ -12,3 +12,27 @@ for (let x = 1; x <= 10; x++) {
     console.log(`${x}* ${y} = ${x * y}`);
   }
 }
+
+let arr = ["Ironman", "Thor", "Hulk"];
+console.log(arr.length);
+
+for (let hero = 0; hero < arr.length; hero++) {
+  const element = arr[hero];
+  console.log(element);
+}
+
+for (let a = 0; a <= 10; a++) {
+  if (a === 5) {
+    console.log("Your last number is 5");
+    break;
+  }
+  console.log(`value of a is ${a}`);
+}
+
+for (let a = 0; a <= 20; a++) {
+  if (a === 10) {
+    console.log("skip 10");
+    continue;
+  }
+  console.log(`value of a is ${a}`);
+}
