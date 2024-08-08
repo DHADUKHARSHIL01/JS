069af6faf7f8f67a6c46bc2280981695f7b06939
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-08 09:22:24 +0530

    18_while_and_doWhile_loop

---
+++ 18_while_and_doWhile_loop.js
@@ -0,0 +1,22 @@
+let index = 0;
+
+while (index <= 10) {
+  console.log(`value of index is ${index}`);
+  index = index + 2;
+}
+
+let hero = ["Ironman", "Thor", "Hulk"];
+
+let range = 0;
+
+while (range < hero.length) {
+  console.log(hero[range]);
+  range++;
+}
+
+let num = 10;
+
+do {
+  console.log(`num is ${num}`);
+  num++;
+} while (num <= 10);
