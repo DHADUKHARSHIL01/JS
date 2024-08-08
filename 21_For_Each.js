b42342f68f31125d2f2d34d2a65ff85fc76c4b1b
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-08 12:51:16 +0530

    21_For_Each

---
+++ 21_For_Each.js
@@ -0,0 +1,31 @@
+const coursName = ["c++", "javascript", "Amazon Web Services", "Ruby"];
+
+coursName.forEach(function (val) {
+  console.log(val);
+});
+
+const myfile = [
+  {
+    languageName: "c++",
+    languageFileName: "cpp",
+  },
+
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+
+  {
+    languageName: "php",
+    languageFileName: "php",
+  },
+
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+];
+
+myfile.forEach((item) => {
+  console.log(item);
+});
