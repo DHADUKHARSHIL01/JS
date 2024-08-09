66798dbbf0bec197962ec1e30aa7508d9908e27d
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-09 12:51:56 +0530

    01/25_try_and_catch

---
+++ 01/25_try_and_catch.js
@@ -0,0 +1,17 @@
+let a = 56;
+let b = 27;
+
+console.log("a =", a);
+console.log("b =", b);
+console.log("a + b =", a + b);
+
+try {
+  console.log("a + b =", a + c);
+} catch (error) {
+  console.log(error);
+}
+
+console.log("a + b =", a + b);
+console.log("a + b =", a + b);
+console.log("a + b =", a + b);
+console.log("a + b =", a + b);
