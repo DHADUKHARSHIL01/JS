0b3cb37ed4a9814bbe7c14d26ad703ea57105dbd
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-14 14:42:49 +0530

    01_task

---
+++ Task/01_task.js
@@ -0,0 +1,9 @@
+const x = 10;
+
+if (x > 0) {
+  console.log("number is positive");
+} else if (x < 0) {
+  console.log("number is negative");
+} else if (x == 0) {
+  console.log("number is zero");
+}
