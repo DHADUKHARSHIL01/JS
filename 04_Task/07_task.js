1011ab34934fbd46b04a12f83febef8575127cc9
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-15 18:25:14 +0530

    07_task

--- 04_Task/07_task.js
+++ 04_Task/07_task.js
@@ -1,26 +1,15 @@
 const score = 99;
 
-if (score >= 93) 
-{
+if (score >= 93) {
   console.log("your grade is A");
-}
-else if (score >= 80)
-{
+} else if (score >= 80) {
   console.log("your grade is B");
-}
-else if (score >= 66)
-{
+} else if (score >= 66) {
   console.log("your grade is C");
-}
-else if (score >= 55)
-{
+} else if (score >= 55) {
   console.log("your grade is D");
-}
-else if (score >= 44)
-{
+} else if (score >= 44) {
   console.log("your grade is E");
-} 
-else if (score >= 33) 
-{
+} else if (score >= 33) {
   console.log("your grade is F");
 }
