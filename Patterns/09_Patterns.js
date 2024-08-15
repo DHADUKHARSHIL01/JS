ea4091afe191cf0b2af4f8423e8cdeb3948bab43
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-15 11:41:24 +0530

    07_Patterns

---
+++ Patterns/09_Patterns.js
@@ -0,0 +1,23 @@
+const num = 5;
+
+for (let x = 0; x < num - 1; x++) {
+  let newal = "";
+  for (let y = 0; y <= x; y++) {
+    newal += " ";
+  }
+  for (let z = 0; z < num - x; z++) {
+    newal += "* ";
+  }
+  console.log(newal);
+}
+
+for (let x = 0; x < num; x++) {
+  let newal = "";
+  for (let y = 0; y < num - x; y++) {
+    newal += " ";
+  }
+  for (let z = 0; z <= x; z++) {
+    newal += "* ";
+  }
+  console.log(newal);
+}
