6f6ab0d26e3b69dd241f2fe062a8737c9d543da2
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-16 18:49:31 +0530

    02_Patterns

--- 05_Patterns/02_Patterns.js
+++ 05_Patterns/02_Patterns.js
@@ -2,11 +2,14 @@ const num = 5;
 
 for (let x = 0; x < num; x++) {
   let newal = " ";
+
   for (let y = 0; y < num - x; y++) {
     newal += " ";
   }
+
   for (let z = 0; z <= x; z++) {
     newal += "*";
   }
+
   console.log(newal);
 }
