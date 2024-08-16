fb059f6f3d68ca07a7edd1729564fdfd1004ad0d
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-16 19:05:51 +0530

    04_task

--- 04_Task/04_task.js
+++ 04_Task/04_task.js
@@ -4,9 +4,11 @@ switch (color) {
   case "red":
     console.log("Please stop your vehicle.");
     break;
+
   case "yellow":
     console.log("Please slow down your vehicle.");
     break;
+
   case "green":
     console.log("Please go.");
     break;
